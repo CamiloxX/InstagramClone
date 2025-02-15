804914921e94f36d73fcd3f457013837ee1750f0
Camilo <camisbarragan2425@gmail.com>
2025-02-15 16:22:06 -0500

    URL DE REGISTER

--- clone_twitter/twitter/urls.py
+++ clone_twitter/twitter/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_view, name='logout'),
-    path('editar/', views.editar_view, name='editar'),  # Add this line
+    path('editar/', views.editar_view, name='editar'),
+    path('delete/<int:post_id>/', views.delete_post, name='delete'),  
+    path('register/', views.register_view, name='register'),
 ]
--- clone_twitter/twitter/views.py
+++ clone_twitter/twitter/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from .models import Profile, Post
+from .forms import UserForm
 
 def home(request):
     posts = Post.objects.all()
     context = {'posts': posts}
     return render(request, 'twitter/newsfeed.html', context)  
+def register_view(request):
+    context = {}
+    return render(request, 'twitter/register.html', context)
 
 def logout_view(request):
     logout(request)
     return redirect('home')
 
 def editar_view(request):
-    # Add your view logic here
     return render(request, 'twitter/editar.html')
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+
+    
+    if request.user == post.user:
+        post.delete()
+
+    return HttpResponseRedirect(reverse('home')) 
